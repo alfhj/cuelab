06af1bd07dc9110431a98bef14e2cb054007be37
Alf Helge Jakobsen <alfern159@gmail.com>
2024-04-14 21:20:11 +0200

    Read cues from file

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
 from src import start_application
 
+CUES_DIR = "cues"
+CUES_FILE = "cues.txt"
+
 if __name__ == "__main__":
-    start_application.run()
+    start_application.run(CUES_DIR, CUES_FILE)
--- src/start_application.py
+++ src/start_application.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from rich.console import Console
 from rich.layout import Layout
 from rich.live import Live
@@ -15,8 +16,25 @@ def make_layout() -> Layout:
     return root
 
 
-def get_filelist() -> list[str]:
-    return [lorem.ipsum() for _ in range(100)]
+def get_filelist(filename: str) -> list[str]:
+    with open(filename, "r", encoding="utf-8") as f:
+        contents = f.read().splitlines()
+    
+    return list(filter(lambda l: not(l.strip() == "" or l.startswith("#")), contents))
+    #return [lorem.ipsum() for _ in range(100)]
+
+
+def get_debug_panel(**kwargs):
+    text = "\n".join(f"{key} = {value}" for key, value in kwargs.items())
+    return Panel(text)
+
+
+def get_book_variable_module_name(module_name):
+    module = globals().get(module_name, None)
+    book = {}
+    if module:
+        book = {key: value for key, value in module.__dict__.iteritems() if not (key.startswith("__") or key.startswith("_"))}
+    return book
 
 
 def get_visible_filelist(filelist: list[str], number_visible: int, selected_index: int, playing_index=None, middle_index: int = None) -> list[str]:
@@ -54,50 +72,59 @@ def get_visible_filelist(filelist: list[str], number_visible: int, selected_inde
     return "\n".join(lines), from_index, local_selected_index, local_playing_index
 
 
-def get_debug_panel(**kwargs):
-    text = "\n".join(f"{key} = {value}" for key, value in kwargs.items())
-    return Panel(text)
+def handle_keypress():
+    global selected_index
+    global playing_index
+    global max_index
+
+    key = readkey()
+   
+    if key == special_keys.UP:
+        selected_index = max(selected_index - 1, 0)
+    if key == special_keys.DOWN:
+        selected_index = min(selected_index + 1, max_index)
+    if key == special_keys.SPACE:
+        if selected_index != playing_index:
+            playing_index = selected_index
+        else:
+            playing_index = min(playing_index + 1, max_index)
+            selected_index = playing_index
+    if key == "w" or key == "W":
+        playing_index = max(playing_index - 1, 0)
+    if key == "s" or key == "S":
+        playing_index = min(playing_index + 1, max_index)
+
+    return key
 
 
-def get_book_variable_module_name(module_name):
-    module = globals().get(module_name, None)
-    book = {}
-    if module:
-        book = {key: value for key, value in module.__dict__.iteritems() if not (key.startswith("__") or key.startswith("_"))}
-    return book
+def update_view(live: Live, layout: Layout, pressed_key: bool = None):
+    global filelist
 
+    height = live.console.height - 2
+    if pressed_key:
+        pressed_key = next((k for k, v in special_keys.__dict__.items() if not pressed_key.startswith("_") and pressed_key == v), pressed_key)
+    visible_filelist, i1, i2, i3 = get_visible_filelist(filelist, height, selected_index, playing_index)
+    layout["left"].update(Panel(visible_filelist))
+    layout["right"].update(get_debug_panel(key=pressed_key, selected_index=selected_index, playing_index=playing_index, fromi=i1, local_s=i2, local_p=i3, height=height))
+    live.refresh()
 
-def run():
-    layout = make_layout()
-    filelist = get_filelist()
+def run(cues_dir, cues_file):
+    global filelist
+    global selected_index
+    global playing_index
+    global max_index
+    
+    cues_path = Path(cues_dir)
+    cues_file_path = cues_path.joinpath(cues_file)
+    filelist = get_filelist(str(cues_file_path))
     selected_index = 0
     playing_index = 0
     max_index = len(filelist) - 1
+    layout = make_layout()
 
-    with Live(layout, refresh_per_second=10, screen=True, auto_refresh=False, vertical_overflow="ellipsis") as live:
-        live.update(layout, refresh=True)
+    with Live(layout, screen=True, auto_refresh=False) as live:
+        update_view(live, layout)
+
         while True:
-            key = readkey()
-            height = live.console.height - 2
-
-            if key == special_keys.UP:
-                selected_index = max(selected_index - 1, 0)
-            if key == special_keys.DOWN:
-                selected_index = min(selected_index + 1, max_index)
-            if key == special_keys.SPACE:
-                if selected_index != playing_index:
-                    playing_index = selected_index
-                else:
-                    playing_index = min(playing_index + 1, max_index)
-                    selected_index = playing_index
-            if key == "w" or key == "W":
-                playing_index = max(playing_index - 1, 0)
-            if key == "s" or key == "S":
-                playing_index = min(playing_index + 1, max_index)
-
-            visible_filelist, i1, i2, i3 = get_visible_filelist(filelist, height, selected_index, playing_index)
-            key_name = next((k for k, v in special_keys.__dict__.items() if not key.startswith("_") and key == v), key)
-
-            layout["left"].update(Panel(visible_filelist))
-            layout["right"].update(get_debug_panel(key=key_name, selected_index=selected_index, playing_index=playing_index, fromi=i1, local_s=i2, local_p=i3, height=height))
-            live.update(layout, refresh=True)
+            key = handle_keypress()
+            update_view(live, layout, key)
